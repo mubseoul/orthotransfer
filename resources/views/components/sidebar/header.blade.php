15de794c4368efd11b8809b3e1cadab3e81ece90
Mubashir Siddiqui <mubupwork@gmail.com>
2025-07-19 01:01:00 +0500

    fix issues

--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -1,7 +1,7 @@
 <div class="px-6 py-4 border-b border-gray-200 bg-sky-50">
     <div class="flex items-center space-x-3">
         <img class="h-10 w-10 rounded-full object-cover border-2 border-sky-200" 
-             src="{{ $user->profile_picture_url }}" 
+             src="{{ $user->profile_picture ?? asset('images/default-avatar.png') }}" 
              alt="Profile picture">
         <div>
             <h2 class="text-lg font-semibold text-gray-900">Dashboard</h2>
